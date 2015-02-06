9b664172abca46da0d250372b79b690c881661d7
Amaresh Pattanaik <apattanaik@walmartlabs.com>
2015-02-06 11:35:40 -0800

    Data Structures and Algorithms examples

---
+++ src/main/java/chapter2/UserDaoImpl.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class UserDaoImpl implements UserDao {
+
+	public User findUserByEmail(String email) {
+		// this guy makes a connection to a DB 
+		// and gets the user object
+		return null;
+	}
+
+}
